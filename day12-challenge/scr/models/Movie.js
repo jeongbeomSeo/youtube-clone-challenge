b6d1c7949415689d7528b1a538fa476209dccbfe
jeongbeomSeo <jeongbeom4693@gmail.com>
2022-01-19 00:05:35 +0900

    Using Mongo DB Easy

---
+++ day12-challenge/scr/models/Movie.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+// Create a Movie Model here.
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  summary: { type: String, required: true },
+  year: { type: Number, required: true },
+  rating: { type: Number, required: true },
+  genres: [{ type: String, required: true }],
+});
